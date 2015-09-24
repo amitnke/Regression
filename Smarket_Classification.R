fc44d029ff9631307925fe0f133c8ffb17e54254
Amit <analystamit@outlook.com>
2015-09-23 22:24:53 -0500

    Updating on 09/23

---
+++ Smarket_Classification.R
@@ -0,0 +1,42 @@
+library(ISLR)
+names(Smarket)
+dim(Smarket)
+summary(Smarket)
+pairs(Smarket)
+cor(Smarket)# Error - x must be numeric
+cor(Smarket[-9])#Produces a matrix that contains all of the pairwise correlations among the predictors in a dataset. 
+# The first command below gives an error message because teh Directiob variable is qualitative. 
+#As one would expect the correlation between the lag variable and today's return are close to zero. 
+#The only substantial relationship is between Year and Volume. 
+attach(Smarket)
+plot(Year, Volume)
+plot(Volume)
+#############################Logistic Regression###############################
+#Logistic regression to predict Direction using Lag1 through Lag5 and Volume.# 
+glm.fit <- glm(Direction~Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume, family = binomial)
+summary(glm.fit)
+#The smallest p value is associated to Lag1. The negative coefficient for this predictor suggest that if the market 
+#had a positive erturn yesterday, then it less likely to go up today. 
+names(glm.fit)
+glm.fit$coefficients # or coef(glm.fit)
+summary(glm.fit)$coefficients # summary(glm.fit)$coef
+#To get p value of the model
+summary(glm.fit)$coefficients[,4]
+###Prediction() function can be used to predict Direction given predictors###
+##The type="response" option tells R to output probabilities of the form P(Y = 1|X), 
+#as opposed to other information such as the logit. 
+glm.probs <- predict(glm.fit, type = "response")##Training data will be used for prediction if no data provided to prediction fucntion. 
+contrasts(Direction)#It indicates that R has created a dummy ariable with 1 for up. 
+#Commands to translate prediction into Up/Down
+glm.pred <- rep("Down", 1250)
+glm.pred[glm.probs > 0.5] = "Up"
+#Confustion matrix to estimate the correct prediction
+table(glm.pred, Direction)
+mean(glm.pred==Direction)#The diagonal elements of the confusion matrix indicate correct predictions. 
+#while the off-diagonals represent incorrect predictions. 
+##############Dividing the training and test data#############################
+train <- (Year < 2005)
+Smarket.2005 = Smarket[!train,]
+dim(Smarket.2005)
+Direction.2005 = Direction[!train]
+#Smarket[train,] is the training data
